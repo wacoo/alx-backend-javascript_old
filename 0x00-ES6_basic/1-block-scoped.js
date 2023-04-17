b6ad04acd7b5d1cd7a91cb55a6ef06e2aa0045eb
wac <wabaham9@gmail.com>
2023-04-17 11:15:44 +0000

    first commit

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+/*
+ * fix give code block scoped
+*/
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+  return [task, task2];
+}
---
+++ 0x00-ES6_basic/8-main.js
@@ -0,0 +1,7 @@
+import getBudgetForCurrentYear from './8-getBudgetCurrentYear.js';
+
+console.log(getBudgetForCurrentYear(2100, 5200, 1090));
+
+bob@dylan:~$
+bob@dylan:~$ npm run dev 8-main.js 
+{ 'income-2021': 2100, 'gdp-2021': 5200, 'capita-2021': 1090 }
