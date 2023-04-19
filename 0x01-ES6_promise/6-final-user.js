5e8a37eb0f8f487fee43d0a715c843ee9602cd7b
wac <wabaham9@gmail.com>
2023-04-19 12:40:53 +0000

    task 6 correction 3

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,14 +3,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promise1 = signUpUser(firstName, lastName);
-  const promise2 = uploadPhoto(fileName);
 
-  return Promise.allSettled([promise1, promise2])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((value) => {
       value.map((val) => ({
         status: val.status,
-        value: val.status === 'fulfilled' ? val.value : String(val.reason),
+        value: val.status === 'fulfilled' ? val.value : val.reason.toStirng(),
       }));
     });
 }
