4a68ac95e3778e33425fcf12113c3f64f06e069e
wac <wabaham9@gmail.com>
2023-04-19 12:33:33 +0000

    task 6 correction

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,9 +1,12 @@
 /* accept two arguments numerator and denominator,
-when denominator is equal to 0, throw new error */
+when denominator is equal to 0, throw new error 
+
+
+*/
 
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
-    throw new Error('cannot devide by 0');
+    throw new Error('cannot divide by 0');
   }
 
   return numerator / denominator;
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -6,7 +6,7 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (e) {
-    queue.push(e.toString());
+    queue.push(String(e));
   } finally {
     queue.push('Guardrail was processed');
   }
