a3ebfa81356cd675bbc816b25d2df3cfedce23b6
wac <wabaham9@gmail.com>
2023-04-19 12:17:59 +0000

    correction 5

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,14 +3,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const promise1 = signUpUser(firstName, lastName);
-  const promise2 = uploadPhoto(fileName);
-
-  return Promise.allSettled([promise1, promise2])
-    .then((value) => {
-      value.map((val) => ({
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => (
+      values.map((val) => ({
         status: val.status,
-        value: val.status === 'fulfilled' ? val.status : val.reason.toString(),
-      }));
-    });
+        value: val.status === 'fulfilled' ? val.value : String(val.reason),
+      }))
+    ));
 }
