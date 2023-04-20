06f17beecda9b0b6b005dd4956b3b641b9cb244d
wac <wabaham9@gmail.com>
2023-04-20 15:09:29 +0000

    task 5 correction

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -4,7 +4,8 @@
 */
 export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
       if (this.evacuationWarningMessage !== 'function') {
         throw new Error('Class extending Building must override evacuationWarningMessage');
       }
@@ -17,6 +18,9 @@ export default class Building {
   }
 
   set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft is not be a number');
+    }
     this._sqft = sqft;
   }
 }
