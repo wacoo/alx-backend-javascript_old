f614329f3d44f3b939a7af3a5828f1a42c2918cf
wac <wabaham9@gmail.com>
2023-04-25 15:45:16 +0000

    task 8 correction

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -7,15 +7,14 @@
 
 export default function cleanSet(set, startString) {
   const str = [];
-  if (set && startString && (set instanceof Set) && typeof startString === 'string') {
-    for (const element of set.values()) {
-      const elt = element.substring(startString.length, element.length);
-      if (element && element.startsWith(startString) && typeof element === 'string' && elt !== element) {
-        str.push(elt);
-      }
-    }
-  } else {
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
+  }
+  for (const element of set.values()) {
+    const elt = element.substring(startString.length, element.length);
+    if (element && element.startsWith(startString) && typeof element === 'string' && elt !== element) {
+      str.push(elt);
+    }
   }
   return str.join('-');
 }
