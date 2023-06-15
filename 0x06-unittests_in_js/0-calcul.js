cef36a52777e16cd6b19d50fcc0d2eb315da993b
wac <wabaham9@gmail.com>
2023-06-15 21:24:38 +0300

    task 0

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,7 @@
+/* acccept two numbers returns the sum */
+
+function calculateNumber(a, b) {
+  return Math.round(a + b);
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,34 @@
+/* test 0-calcul.caculateNumber */
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('tests caculateNumber', () => {
+  it('test', () => {
+    const sum = calculateNumber(2, 4);
+    assert.strictEqual(sum, 6);
+  });
+  it('test', () => {
+    const sum = calculateNumber(2.2, 4.2);
+    assert.strictEqual(sum, 6);
+  });
+  it('test', () => {
+    const sum = calculateNumber(2.6, 4.2);
+    assert.strictEqual(sum, 7);
+  });
+  it('test', () => {
+    const sum = calculateNumber(2, -4);
+    assert.strictEqual(sum, -2);
+  });
+  it('test', () => {
+    const sum = calculateNumber(4, -2);
+    assert.strictEqual(sum, 2);
+  });
+  it('test', () => {
+    const sum = calculateNumber(2.2, -4.2);
+    assert.strictEqual(sum, -2);
+  });
+  it('test', () => {
+    const sum = calculateNumber(-2.6, 4.2);
+    assert.strictEqual(sum, 2);
+  });
+});
