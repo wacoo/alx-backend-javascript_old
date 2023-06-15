357910d7e2f6e0f1430e7166e3cd1edfbf322d39
wac <wabaham9@gmail.com>
2023-06-15 23:04:51 +0300

    task 2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -4,11 +4,13 @@ const expect = require("chai").expect;
 const calculateNumber = require("./1-calcul");
 
 describe("calculateNumber()", function() {
-
+  describe("SUM", function() {
     it(`checking if numbers round`, function() {
       const res = calculateNumber('SUM', 1, 3);
       expect(res).to.equal( 4);
     });
+  });
+  describe("SUBTRACT", function() {
     it(`checking if numbers round`, function() {
       const res = calculateNumber('SUBTRACT', 1.3, 2.7);
       expect(res).to.equal(-2);
@@ -17,6 +19,8 @@ describe("calculateNumber()", function() {
       const res = calculateNumber('SUBTRACT', 4.8, 2.8);
       expect(res).to.equal(2);
     });
+  });
+  describe("DIVIDE", function() {
     it(`checking if numbers round`, function() {
       const res = calculateNumber('DIVIDE', 4, 2);
       expect(res).to.equal(2);
@@ -29,4 +33,5 @@ describe("calculateNumber()", function() {
       const res = calculateNumber('DIVIDE', 1.3, 4.7);
       expect(res).to.equal(0.2);
     });
+  });
 });
